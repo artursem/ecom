532392bf0b8877eb8b342a9994eb90afa60ff558
artursem <artur.semeniuk@gmail.com>
2021-04-30 20:57:42 +0200

    add express module

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hi there!');
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email">
+            <input name="password" placeholder="password">
+            <input name="passwordconfirmation" placeholder="password confirmation">
+            <button>Sign Up</button>  
+        </form>
+    </div>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Account created');
 });
 
 app.listen(3000, () => {
     console.log('Listening');
-})
+});
