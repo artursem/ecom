e989d32083dee0ce7e0c5ce28b3a2b2fe4483c17
artursem <artur.semeniuk@gmail.com>
2021-05-01 22:01:17 +0200

    add creating new users

--- repositories/users.js
+++ repositories/users.js
@@ -14,14 +14,25 @@ class UsersRepository {
         }
     }
     async getAll() {
-        return JSON.parse(await fs.promises.readFile(this.filename, {
+        return JSON.parse(
+            await fs.promises.readFile(this.filename, {
             encoding: 'utf8'
         }));
+    }
+
+    async create(attrs) {
+        const records = await this.getAll();
+        records.push(attrs);
+
+        await fs.promises.writeFile(this.filename, JSON.stringify(records));
     }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
+    await repo.create(
+        {email: 'test@test.com', password: 'qwerty'}
+    );
     const users = await repo.getAll();    
     console.log(users);
 }
