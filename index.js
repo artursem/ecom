a29f4812ccf71ba3af07998125055c6df6b3e02e
artursem <artur.semeniuk@gmail.com>
2021-05-06 18:24:17 +0200

    added general repository

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
@@ -12,7 +14,9 @@ app.use(
   })
 );
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log('Listening');
 });
+
